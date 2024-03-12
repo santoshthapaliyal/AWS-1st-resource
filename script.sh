fd46c85081e6c9ec1dff484145697fea6072a4bb
santoshthapaliyal <51606639+santoshthapaliyal@users.noreply.github.com>
2024-03-12 08:48:48 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt update 
 sudo apt install nginx -y 
-sudo echo "Hi Gaurav sir" >/var/www/html/index.nginx-debian.html
+sudo echo "Welcome to Nginx" >/var/www/html/index.nginx-debian.html
